6f55f00cf7c0911d05b9aa603c73ea0c8610d078
Felipe Pereira <felipe.pereira64@fatec.sp.gov.br>
2024-10-17 10:36:41 -0300

    feat: att frontend com formulário

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Form from "./components/Form";
+import Grid from "./components/Grid";
+import { useState } from "react";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -13,13 +17,30 @@ const Container = styled.div`
   gap: 10px;
 `;
 
+const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try{
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  }
+
   return (
     <>
       <Container>
+        <Title>USUÁRIOS</Title>
+        <Form />
+        <Grid />
       </Container>
-      <ToastContainer autoClose={3500} position={toast.POSITION.TOP.RIGHT} />
+      <ToastContainer autoClose={3500} position="top-right" />
+
       <GlobalStyle />
     </>
   );
